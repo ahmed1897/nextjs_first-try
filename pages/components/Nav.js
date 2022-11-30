6d6b63ddcd24835fed5f26b6d9ad67deda51c100
ahmed1897 <ahmed1897@gmail.com>
2022-11-30 11:57:24 +0200

    remove README

---
+++ pages/components/Nav.js
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import { auth } from "../../utils/firebase";
+import {useAuthState} from "react-firebase-hooks/auth";
+import Image from 'next/image';
+
+export default function Nav() {
+  const [user, loading] = useAuthState(auth);
+
+  return (
+    <nav className="flex justify-between items-center py-10">
+      <Link href="/">
+        <button className="text-lg font-medium">El home ya Bany Adam</button>
+      </Link>
+      <ul className="flex items-center gap-10">
+        {!user && (
+        <Link
+          href="/auth/login"
+          className="py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8"
+        >
+          Dos hna kda
+        </Link>
+        )}
+        {user && (
+          <div className="flex items-center gap-6">
+            <Link href="/post">
+              <button className="font-medium bg-cyan text-white py-2 px-4 rounded-mg text-sm">Post</button>
+            </Link>
+            <Link href="/dashboard">
+            <Image className="w-12 rounded-full cursor-pointer" src={user.photoURL} alt="" width={500} height={500} />
+            </Link>
+          </div>
+
+        )}
+      </ul>
+    </nav>
+  );
+}
